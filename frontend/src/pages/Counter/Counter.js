398e8d292319e325c8af637cc942873cbb8c0aaa
kazemammar <kazemjulien.ammar@gmail.com>
2022-04-11 21:41:30 +0200

    KA: Added Movie/User MM

--- frontend/src/pages/Counter/Counter.js
+++ frontend/src/pages/Counter/Counter.js
@@ -2,15 +2,17 @@ import { useState } from 'react';
 import './Counter.css';
 
 function Counter() {
-  const [counter, setCounter] = useState(0);
+    const [counter, setCounter] = useState(0);
 
-  return (
-    <div className="Counter-container">
-      <h1>This is a counter example</h1>
-      <div>Counter value : {counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>Increment counter</button>
-    </div>
-  );
+    return (
+        <div className="Counter-container">
+            <h1>This is a counter example</h1>
+            <div>Counter value : {counter}</div>
+            <button onClick={() => setCounter(counter + 1)}>
+                Increment counter
+            </button>
+        </div>
+    );
 }
 
 export default Counter;
