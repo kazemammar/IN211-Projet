5e9db0e977536a456463ad18af1f38b5be941f26
kazemammar <kazemjulien.ammar@gmail.com>
2022-04-20 10:07:35 +0200

    Added New Variable settings

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,14 +1,19 @@
 module.exports = {
   type: "postgres",
-  host: "database-1.cmxb7wpkjrv9.eu-west-1.rds.amazonaws.com",
-  port: 5432,
-  username: "postgres",
-  password: process.env.DATABASE_PASSWORD,
-  database: "kazem",
+  host: process.env.host,
+  port: process.env.port,
+  username: process.env.username,
+  password: process.env.password,
+  database: process.env.database,
   synchronize: false,
   entities: ["entities/*.js"],
   migrations: ["migrations/*.js"],
   cli: {
     migrationsDir: "migrations",
+  },
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 };
