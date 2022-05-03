49c93cc8e74a73c2503efa13549e6dd20fd072ee
Celestin-Cordos <celestin.cordos@ensta-paris.fr>
2022-05-03 11:45:11 +0200

    le frontend est à peu pres fini, je vais l'intégrer avec le back, pour afficher les films de notre base de données à nous

---
+++ frontend/src/pages/Movies/SingleMovie.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import Paper from '@mui/material/Paper';
+import './SingleMovie.js';
+import { Container, Grid } from '@mui/material';
+import axios from 'axios';
+import MovieDetails from '../../components/Movies/MovieDetails.js';
+
+export function SingleMovie(props) {
+    console.log(props);
+    const { id } = props;
+
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get(`${process.env.REACT_APP_BACKDEND_URL}/${id}`)
+            .then(res => {
+                setMovie(res.data);
+            })
+            .catch(error => console.log(error));
+    }, []);
+
+    return (
+        <>
+            {/* <Container maxWidth="lg"> */}
+            <MovieDetails movie={movie}></MovieDetails>
+            {/* </Container> */}
+        </>
+    );
+}
