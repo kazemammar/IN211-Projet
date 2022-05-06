ead912c09babbb3d0a060a855e814e6d55988f85
Celestin-Cordos <celestin.cordos@ensta-paris.fr>
2022-05-06 18:11:25 +0200

    je peux faire des requetes pour ajouter une review par user et par film et les recuperer par user (à changer: par film ou par film et user). En revanche, on peut créer plsrs reviews pour un film et unmovie

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -5,9 +5,9 @@ const User = require("../entities/user");
 const Movie = require("../entities/movie");
 const router = express.Router();
 
-router.get("/", function (req, res) {
+router.get("/:user_id/", function (req, res) {
   getRepository(Review)
-    .find({})
+    .find({ relations: ["user"], user_id: req.params.user_id })
     .then(function (reviews) {
       res.json({ reviews: reviews });
     });
@@ -15,47 +15,56 @@ router.get("/", function (req, res) {
 
 router.post("/new", function (req, res) {
   const reviewRepository = getRepository(Review);
-  const users = getRepository(User).find({ id: req.body.user });
-  const movies = getRepository(Movie).find({ id: req.body.movie });
-  var user = null;
-  var movie = null;
-  if (users.length > 0) {
-    user = users[0];
-  } else {
-    res.status(401).json({
-      message: "This user does not exist, please connect to post review",
-    });
-    return;
-  }
-  if (users.length > 0) {
-    movie = movies[0];
-  } else {
-    res.status(404).json({
-      message: "This movie does not exist, please select a valid movie",
-    });
-    return;
-  }
-  const newReview = reviewRepository.create({
-    mark: req.body.mark,
-    comment: req.body.comment,
-    user: user,
-    movie: movie,
-  });
+  getRepository(User)
+    .find({ id: req.body.user })
+    .then((users) => {
+      getRepository(Movie)
+        .find({ id: req.body.movie })
+        .then((movies) => {
+          var user = null;
+          var movie = null;
+          if (users.length > 0) {
+            user = users[0];
+          } else {
+            res.status(401).json({
+              message:
+                "This user does not exist, please connect to post review",
+            });
+            return;
+          }
+          if (users.length > 0) {
+            movie = movies[0];
+          } else {
+            res.status(404).json({
+              message: "This movie does not exist, please select a valid movie",
+            });
+            return;
+          }
+          const newReview = reviewRepository.create({
+            mark: req.body.mark,
+            comment: req.body.comment,
+            user: user,
+            movie: movie,
+          });
 
-  reviewRepository
-    .insert(newReview)
-    .then(function (newDocument) {
-      res.status(201).json(newDocument);
-    })
-    .catch(function (error) {
-      console.error(error);
-      if (error.code === "23505") {
-        res.status(400).json({
-          message: `You already have a review on this movie`,
+          reviewRepository
+            .insert(newReview)
+            .then(function (newDocument) {
+              res.status(201).json(newDocument);
+            })
+            .catch(function (error) {
+              console.error(error);
+              if (error.code === "23505") {
+                res.status(400).json({
+                  message: `You already have a review on this movie`,
+                });
+              } else {
+                res
+                  .status(500)
+                  .json({ message: "Error while creating the user" });
+              }
+            });
         });
-      } else {
-        res.status(500).json({ message: "Error while creating the user" });
-      }
     });
 });
 
